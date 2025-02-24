8fd9aefb339ccaeb271d0c4d98c53e12da512f1e
ttetricas <estermeireles89@gmail.com>
2025-02-23 23:02:40 -0300

    Exercício Resolvido 2

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Peruíbe");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Cubatão");
+Console.WriteLine("Santos");
+Console.WriteLine("Guarujá");
